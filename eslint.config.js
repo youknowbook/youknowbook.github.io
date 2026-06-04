3f9af3ab399c6acdc3cc99b25cb055b47544b3f1
Ilyés Alex <AlexIlyes>
2026-06-04 14:00:05 +0200

    fixed overriding meeting book

--- eslint.config.js
+++ eslint.config.js
@@ -26,4 +26,10 @@ export default defineConfig([
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
+  {
+    files: ['scripts/**/*.{js,mjs}'],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
 ])
